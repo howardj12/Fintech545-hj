0d18341ee6d2299ad4b483664d767a9b8f9ba7e4
Howard Jin <howard.jin@duke.edu>
2023-10-07 22:54:19 -0400

    redodo

---
+++ qrm_hj/build/lib/qrm_hj/__init__.py
@@ -0,0 +1,2 @@
+from . import MC
+from . import VaR
---
+++ qrm_hj/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qrm_hj',
+    version='0.3.2',
+    author='Howard Jin',
+    description='Quatitative Risk Management',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
